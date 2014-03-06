6bdefd3de345947bad00f739e483fdc5347a8529
Daniel Buckle <danielbuckle31@gmail.com>
2014-03-06 11:30:25 +0000

    Changed the betting UI to add more instructions

--- src/ui/betting_ui/BettingSouthPanel.java
+++ src/ui/betting_ui/BettingSouthPanel.java
@@ -5,9 +5,12 @@ import java.awt.event.ActionListener;
 
 public class BettingSouthPanel extends JPanel {
     public final JButton closeButton;
+    public final JTextField disclaimer;
 
     public BettingSouthPanel() {
 
+        disclaimer = new JTextField("Disclaimer: The creator of this application holds no responsibility to any money " +
+        "lost with the odds shown");
         closeButton = new JButton();
         closeButton.setText("Close");
 

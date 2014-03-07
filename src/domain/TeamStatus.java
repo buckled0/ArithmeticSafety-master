46f4de71e9167e9601a1b2524430ce470699481a
Daniel Buckle <danielbuckle31@gmail.com>
2014-03-07 12:33:36 +0000

    Initial commit Changed the initial location so more central for the user More changes to the UI prior to the Demo

--- src/domain/TeamStatus.java
+++ src/domain/TeamStatus.java
@@ -1,7 +1,24 @@
 package domain;
 
 public enum TeamStatus {
-    atRisk, fairlySafeForNow, definitelySafeForNow, equalButTop, equalOnEverything, chanceOfChampion,
-    chanceOfChampionsLeague, chanceOfEuropaLeague, champions, couldBeRelegated, highChanceOfRelegation, definitelyRelegated,
-    drawingForTitle, championsLeague, europaLeague, finalPlace, startOfSeason, onlyTeamInLeague, definitelySafeOfRelegation, chanceOfRelegation, definitelySafe, canGetOutOfRelegation;
+    atRisk,
+    fairlySafeForNow,
+    definitelySafeForNow,
+    equalButTop,
+    equalOnEverything,
+    chanceOfChampion,
+    chanceOfChampionsLeague,
+    chanceOfEuropaLeague,
+    champions,
+    couldBeRelegated,
+    highChanceOfRelegation,
+    definitelyRelegated,
+    drawingForTitle,
+    championsLeague,
+    europaLeague,
+    finalPlace,
+    startOfSeason,
+    definitelySafeOfRelegation,
+    definitelySafe,
+    canGetOutOfRelegation;
 }
